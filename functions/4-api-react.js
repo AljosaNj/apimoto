f811036b57edb277b3265319d179c9e9177b6756
AljosaNj <aljosaa.njezic@gmail.com>
2021-11-30 23:15:42 +0100

    first deploy

---
+++ functions/4-api-react.js
@@ -0,0 +1,35 @@
+require('dotenv').config()
+const Airtable = require('airtable-node');
+ 
+const airtable = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY })
+  .base('app4A9NZ8XRqLdtxW')
+  .table('products')
+
+
+
+exports.handler = async(event,context,cb) => {
+try {
+ const {records} = await airtable.list()
+ const products = records.map((product) => {
+  const {id} = product;
+  const {name,images,price,featured,colors,company,description,category,registration} = product.fields
+  const image = images[0].url
+  return {id,name,image,price,featured,colors,company,description,category,registration}
+ })
+  return {
+headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+
+ statusCode: 200,
+  body: JSON.stringify(products),
+ }
+} catch (error) {
+  return {
+ statusCode: 500,
+  body: 'Server Error',
+ }
+}
+ 
+
+}
---
+++ functions/4-z-complete.js
@@ -0,0 +1,53 @@
+require('dotenv').config()
+const Airtable = require('airtable-node');
+ 
+const airtable = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY })
+  .base('app4A9NZ8XRqLdtxW')
+  .table('products')
+
+  exports.handler = async (event, context, cb) => {
+  const { id } = event.queryStringParameters
+  if (id) {
+
+
+
+    try {
+      const product = await airtable.retrieve(id)
+      
+       
+      if (product.error) {
+        return {
+          statusCode: 404,
+          body: `No product with id: ${id}`,
+        }
+      }
+      return {
+        statusCode: 200,
+        body: JSON.stringify(product),
+      }
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: `Server Error`,
+      }
+    }
+  }
+  try {
+    const { records } = await airtable.list()
+    const products = records.map((product) => {
+      const { id } = product
+      const { name, images, price,registration,company ,featured,category,colors} = product.fields
+      const image = images[0].url
+      return { id, name, image, price,registration,company,featured,category ,colors}
+    })
+    return {
+      statusCode: 200,
+      body: JSON.stringify(products),
+    }
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: 'Server Error',
+    }
+  }
+}
